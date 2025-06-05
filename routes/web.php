8cc8b0fb1328a527fbe49279f35cd5d31b842ce5
Phuriphut Phokasawat <phuriphut2002@yahoo.com>
2025-06-05 12:30:49 +0700

    Fix some UX

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/api/plant-types', [FarmController::class, 'getPlantTypes']);
     Route::post('/api/generate-planting-points', [FarmController::class, 'generatePlantingPoints']);
     Route::post('/api/get-elevation', [FarmController::class, 'getElevation']);
+    Route::post('/api/plant-points/add', [FarmController::class, 'addPlantPoint'])->name('plant-points.add');
+    Route::post('/api/plant-points/delete', [FarmController::class, 'deletePlantPoint'])->name('plant-points.delete');
+    Route::post('/api/plant-points/move', [FarmController::class, 'movePlantPoint'])->name('plant-points.move');
 });
 
 require __DIR__.'/settings.php';
